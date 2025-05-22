9abd5c8e62c8557dc072fb4229b9c4f9701c9daa
EmilioFernd <emiliomuerte282001@gmail.com>
2025-05-22 18:56:34 +0200

    Hemos subido todo el workspace

---
+++ build/motion/CMakeFiles/Controller.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Controller.dir/src/Controller.cpp.o"
+  "CMakeFiles/Controller.dir/src/Controller.cpp.o.d"
+  "Controller"
+  "Controller.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Controller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/motion/ament_cmake_symlink_install_targets_0_.cmake
@@ -0,0 +1,1 @@
+ament_cmake_symlink_install_targets("TARGET_FILES" "/home/ros2/ros/Miniproject_equipo1_ws/build/motion/Controller" "TARGETS" "Controller" "DESTINATION" "lib/motion")
